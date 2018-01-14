4dbdb2c7ea2327fa10fd4f4e02978ef0577b334e
Benjamin Skinner <btskinner@virginia.edu>
2018-01-14 15:35:16 -0500

    init download branch with only scripts and data

--- _modules/knit_setup.R
+++
@@ -1,5 +0,0 @@
-knitr::opts_chunk$set(base.url='../',
-                      fig.path='figures/',
-                      dpi=90,
-                      out.width='100%',
-                      error = TRUE)
